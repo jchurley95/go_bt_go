761db604221ca297001880686538f436a6815d1a
jchurley95 <jchurley95@gmail.com>
2017-09-08 17:09:01 -0400

    Added Event model with name, description, event_time_date which is datetime data type, and points which is integer data type. Created 4 Events in seed file as well as destroy_all, left datetime blank for now, going to pull in datetime_select gem for admin's event form

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Event.destroy_all
+
+Event.create({name: "JV Softball", description: "JV Softball vs. Marist", points: 10})
+Event.create({name: "Freshman Basketball", description: "Freshman Basketball vs. St. Pius X", points: 10})
+Event.create({name: "Varisty Football", description: "Varisty Football vs. Buford", points: 10})
+Event.create({name: "School Play", description: "Sweeney Todd", points: 10})
